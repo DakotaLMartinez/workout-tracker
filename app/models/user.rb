3f2e6f12dadbdbea4349d7a02fc1b1e90c55c988
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-21 13:46:57 -0800

    adds exercises association through workout_exercises to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :workouts
+  has_many :workout_exercises
+  has_many :exercises, through: :workout_exercises
 end
