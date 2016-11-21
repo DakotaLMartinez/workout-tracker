0176264ad290169bf083b78cde545f28edc975fc
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-21 01:14:16 -0800

    adds user_id foreign key to workouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121074150) do
+ActiveRecord::Schema.define(version: 20161121085732) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20161121074150) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
 end
