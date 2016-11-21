1d87fc23da267a7a02a4073241deef116785ce02
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-21 12:12:23 -0800

    adds exercises table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121085732) do
+ActiveRecord::Schema.define(version: 20161121095817) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
