539f4fa54fb3e05e93061fba7a522dde146a5fe8
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-23 14:37:43 -0800

    adds exercise set pundit policy

---
+++ app/policies/exercise_set_policy.rb
@@ -0,0 +1,23 @@
+class ExerciseSetPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user: @user)
+      end
+    end
+  end
+
+  def edit? 
+    user.admin? || record.user == user
+  end
+
+  def update? 
+    user.admin? || record.user == user
+  end
+
+  def destroy? 
+    user.admin? || record.user == user
+  end
+end
