353429cf766a2beb5550dade3e6fedb88b4cd011
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-22 18:44:20 -0800

    adds handling for json format to pundit unauthorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized(exception) 
     policy_name = exception.policy.class.to_s.underscore
-    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
-    redirect_to(request.referrer || root_path)
+    message = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+    flash[:alert] = message
+    respond_to do |format| 
+      format.html { redirect_to(request.referrer || root_path) }
+      format.json { render json: messsage, status: :unauthorized }
+    end
   end
 end
