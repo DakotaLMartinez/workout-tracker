d302e5694b24dcde4e975aced44705d30db78cbf
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-22 17:36:05 -0800

    adds link to toggle admin privileges

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :exercises
   resources :workouts
   devise_for :users
+
+  patch 'users/:id/toggle_admin', to: 'users#update', as: 'toggle_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
