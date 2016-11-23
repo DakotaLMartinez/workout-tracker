b5931f1a3a7fd2b6ef5ce9d0ef24c48986c20cab
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-22 19:17:57 -0800

    adds exercise policy

---
+++ app/policies/exercise_policy.rb
@@ -0,0 +1,29 @@
+class ExercisePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user: @user)
+      end
+    end
+  end
+
+  def edit?
+    admin_or_mine
+  end
+
+  def update?
+    admin_or_mine
+  end
+
+  def destroy? 
+    user.admin?
+  end
+
+  private
+
+  def admin_or_mine 
+    user.admin? || record.user == user
+  end
+end
