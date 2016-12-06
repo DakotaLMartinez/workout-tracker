00eb53f7d5dfb87960c795ad087ec04649745098
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-12-06 12:02:11 -0800

    removes toggle admin button from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'workouts#index'
-  resources :workouts
+  resources :workouts 
+
   resources :exercises do 
     resources :exercise_sets
+        resources :workout_exercises, only: [:create, :destroy]
   end
 
-  patch 'users/:id/toggle_admin', to: 'users#update', as: 'toggle_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
