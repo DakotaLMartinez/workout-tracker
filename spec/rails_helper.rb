7495ea75a976959b2816175948e79460d8dda900
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-12-06 17:23:41 -0800

    adds capybara/poltergeist to rails helper so specs will run

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist 
+Capybara.raise_server_errors = false
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -26,8 +30,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Capybara.javascript_driver = :poltergeist 
-Capybara.raise_server_errors = false
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -47,6 +49,12 @@ RSpec.configure do |config|
 
   config.before(:each) do 
     DatabaseCleaner.start
+  end
+
+  config.include Warden::Test::Helpers
+
+  config.after(:each) do 
+    Warden.test_reset!
   end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
