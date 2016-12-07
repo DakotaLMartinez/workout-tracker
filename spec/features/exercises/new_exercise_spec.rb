311c65a26b8604dc48c01ff823e51aec9da824bf
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-12-06 18:43:34 -0800

    adds New Exercise Spec

---
+++ spec/features/exercises/new_exercise_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'Creating New Exercises' do
+  before do 
+    @user = FactoryGirl.create(:user)
+    visit new_user_session_path 
+    LoginPage.new.sign_in(@user.email, @user.password)
+  end
+
+  scenario 'User creates a new Exercises' do
+    visit new_exercise_path
+    fill_in 'name', with: 'My New Exercise'
+    click_button 'Submit'
+
+    expect(page).to have_text('My New Exercise')
+  end
+
+  scenario 'User creates a new Exercise and assigns it to a workout' do 
+    workout = @user.workouts.create(name: "My New Workout")
+    visit new_exercise_path 
+    fill_in 'name', with: 'My Second Exercise'
+    find('#workout_id').find(:option, workout.name).select_option
+    click_button 'Submit'
+
+    visit workout_path(workout)
+    expect(page).to have_text('My Second Exercise') 
+  end
+end
