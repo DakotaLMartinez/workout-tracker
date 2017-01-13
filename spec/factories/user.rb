b6f4832023ece640b554c0b1a9057bc1c6cd0af3
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-01-13 12:55:57 -0800

    adds user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,22 +4,22 @@ FactoryGirl.define do
     password { Faker::Internet.password(8) }
     password_confirmation { password }
 
-    factory :confirmed_user do
-      confirmed_at Time.zone.now
-    end
+    # factory :confirmed_user do
+    #   confirmed_at Time.zone.now
+    # end
 
     factory :test_user do 
       email { 'test@example.com' }
       password { 'password' }
       password_confirmation { 'password' }
-      confirmed_at Time.zone.now
+      # confirmed_at Time.zone.now
     end
 
     factory :other_user do 
       email { 'other@email.com' }
       password { 'password' }
       password_confirmation { 'password' }
-      confirmed_at Time.zone.now
+      # confirmed_at Time.zone.now
     end
   end
 end
