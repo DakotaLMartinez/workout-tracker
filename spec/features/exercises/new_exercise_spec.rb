86434bac6f80258c52364a83978325d498a11959
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-02-03 19:42:08 -0800

    fixes broken new_exercise_spec

--- spec/features/exercises/new_exercise_spec.rb
+++ spec/features/exercises/new_exercise_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'Creating New Exercises' do
-  before do 
+  before(:each) do 
     @user = FactoryGirl.create(:user)
     visit new_user_session_path 
     LoginPage.new.sign_in(@user.email, @user.password)
@@ -9,16 +9,27 @@ feature 'Creating New Exercises' do
 
   scenario 'User creates a new Exercises' do
     visit new_exercise_path
-    fill_in 'name', with: 'My New Exercise'
+    fill_in 'exercise_name', with: 'My New Exercise'
     click_button 'Submit'
 
     expect(page).to have_text('My New Exercise')
   end
 
+  scenario 'User creates a new Exercise with a description', js:true do 
+    visit new_exercise_path
+    fill_in 'exercise_name', with: 'My Awesome Exercise'
+    fill_in 'exercise_description', with: 'The best exercise ever'
+    click_button 'Submit' 
+    
+    visit exercise_path(Exercise.last)
+    click_link 'Show Details'
+    expect(page).to have_content('The best exercise ever')
+  end
+
   scenario 'User creates a new Exercise and assigns it to a workout' do 
     workout = @user.workouts.create(name: "My New Workout")
     visit new_exercise_path 
-    fill_in 'name', with: 'My Second Exercise'
+    fill_in 'exercise_name', with: 'My Second Exercise'
     find('#workout_id').find(:option, workout.name).select_option
     click_button 'Submit'
 
